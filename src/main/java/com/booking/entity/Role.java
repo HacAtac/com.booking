bfc1e511928fd570e6ba798637c1c6a9686966f9
HacAtac <jhackworth00@outlook.com>
2023-02-27 19:53:45 -0600

    Create User and Role entity and did ManyToMany mapping to create related table. JPA repository next. Just moving from in memory to DB Persistance

---
+++ src/main/java/com/booking/entity/Role.java
@@ -0,0 +1,23 @@
+package com.booking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
