da2a0ce4cbd651f1e89771ca83ca6c4c45fe4455
HacAtac <jhackworth00@outlook.com>
2023-03-01 12:55:45 -0600

    Step 4 complete. Generates JWT token, returns username from token, and Validates token

---
+++ src/main/java/com/booking/security/JwtTokenProvider.java
@@ -0,0 +1,74 @@
+package com.booking.security;
+
+import com.booking.exceptions.ApiException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+    @Value("${app.jwt.expirationDate}")
+    private long jwtExpirationDate;
+
+    //genereate JWT token
+    public String generateToken(Authentication authentication){
+
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+        return token;
+    }
+    private Key key() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+    /// Return username from JWT token
+    public String getUsername(String token){
+       Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        String username = claims.getSubject();
+        return username;
+    }
+
+    // Validate JWT token
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (MalformedJwtException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Expired JWT token");
+        } catch (UnsupportedJwtException ex){
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "JWT claims string is empty.");
+        }
+    }
+
+}
