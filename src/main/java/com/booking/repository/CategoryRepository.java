2152e530153c31a568e691c96a3880865945ad4e
HacAtac <jhackworth00@outlook.com>
2023-03-01 14:23:06 -0600

    Category DTO object + repository

---
+++ src/main/java/com/booking/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.booking.repository;
+
+import com.booking.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+}
